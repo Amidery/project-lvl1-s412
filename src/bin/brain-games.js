7d73b23b29f4a25888db1356e953fc512aea802a
Amidery <zhenev.eva.me@gmail.com>
2019-01-17 12:14:55 +0700

    update code location

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,6 @@
 #! /usr/bin/env node
-import greetings from '..';
+import { greetings, askName } from '..';
 
 greetings();
+console.log();
+askName();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
 import readlineSync from 'readline-sync';
 
-const greetings = () => {
+export const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+};
+
+export const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${userName}!`);
   console.log();
   return userName;
 };
 
-export default greetings;
+export const answerAnalysis = (number, answer) => {
+  let result = '';
+
+  if ((number % 2 === 0 && answer === 'yes') || (number % 2 !== 0 && answer === 'no')) {
+    result = 'Correct!';
+  }
+
+  if (number % 2 === 0 && answer === 'no') {
+    result = '"no" is wrong answer ;(. Correct answer was "yes".';
+  }
+
+  if (number % 2 !== 0 && answer === 'yes') {
+    result = '"yes" is wrong answer ;(. Correct answer was "no".';
+  }
+
+  if (answer !== 'yes' && answer !== 'no') {
+    result = 'Incorrect input. You need to answer yes/no';
+  }
+
+  return result;
+};
+
+export const gameEven = () => {
+  greetings();
+  console.log('Answer "yes" if number even otherwise answer "no"');
+  console.log();
+  const userName = askName();
+
+  const game = () => {
+    const steps = 0;
+
+    const iter = (acc) => {
+      if (acc === 3) {
+        console.log(`Congratulations, ${userName}!`);
+        return;
+      }
+
+      const number = Math.floor(Math.random() * 101);
+      console.log(`Question: ${number}`);
+      const answer = readlineSync.question('Your answer: ');
+
+      if (answerAnalysis(number, answer) === 'Correct!') {
+        console.log(answerAnalysis(number, answer));
+        iter(acc + 1);
+      } else {
+        console.log(answerAnalysis(number, answer));
+        console.log(`Let's try again, ${userName}!`);
+      }
+    };
+
+    return iter(steps);
+  };
+
+  game();
+};
