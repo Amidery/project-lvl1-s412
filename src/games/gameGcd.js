e9513ff7b4fb94d6065d7d43edf6394aab2ec0f0
Amidery <zhenev.eva.me@gmail.com>
2019-01-18 16:46:43 +0700

    add gcd-game

---
+++ src/games/gameGcd.js
@@ -0,0 +1,24 @@
+import { cons } from 'hexlet-pairs';
+import gameLogic from '..';
+import generateNum from '../utils';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const calcGcd = (num1, num2) => {
+  if (num2 === 0) {
+    return num1;
+  }
+
+  return calcGcd(num2, num1 % num2);
+};
+
+const game = () => {
+  const operand1 = generateNum(0, 100);
+  const operand2 = generateNum(0, 100);
+  const question = `${operand1} ${operand2}`;
+  const correctAnswer = calcGcd(operand1, operand2);
+
+  return cons(question, correctAnswer);
+};
+
+export default () => gameLogic(description, game);
