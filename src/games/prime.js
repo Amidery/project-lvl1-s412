938fb1dc3c25c38c05e1a1b4ec49f7f4fbc29675
Amidery <zhenev.eva.me@gmail.com>
2019-01-19 18:27:52 +0700

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import { cons } from 'hexlet-pairs';
+import gameLogic from '..';
+import generateNum from '../utils';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const game = () => {
+  const question = generateNum(0, 1000);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return cons(question, correctAnswer);
+};
+
+export default () => gameLogic(description, game);
