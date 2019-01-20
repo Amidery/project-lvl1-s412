9768e5d74c4c6466b2dbfebc592d31670aba0d32
Amidery <zhenev.eva.me@gmail.com>
2019-01-20 14:27:55 +0700

    update file

--- src/games/progression.js
+++ src/games/progression.js
@@ -9,7 +9,7 @@ const progressionLength = 10;
 const calcProgression = (firstElement, step, hiddenElementPosition) => {
   let progression = '';
 
-  for (let i = 0; i <= progressionLength; i += 1) {
+  for (let i = 0; i < progressionLength; i += 1) {
     if (i === hiddenElementPosition) {
       progression += '.. ';
     } else {
@@ -23,7 +23,7 @@ const calcProgression = (firstElement, step, hiddenElementPosition) => {
 const game = () => {
   const firstElement = generateNum(0, 100);
   const step = generateNum(1, 50);
-  const hiddenElementPosition = generateNum(1, progressionLength);
+  const hiddenElementPosition = generateNum(0, progressionLength - 1);
 
   const correctAnswer = firstElement + step * hiddenElementPosition;
   const question = calcProgression(firstElement, step, hiddenElementPosition);
